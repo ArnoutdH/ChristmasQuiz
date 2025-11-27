95e36f98ec056e45cb734752cf09ca0b0908bdbb
ArnoutdH <147606241+ArnoutdH@users.noreply.github.com>
2025-11-27 17:36:24 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -64,7 +64,9 @@ def main():
         st.session_state.auth2 = False
     if "auth3" not in st.session_state:
         st.session_state.auth3 = False
-    
+    if "auth4" not in st.session_state:
+        st.session_state.auth4 = False
+        
     if not st.session_state.auth0:
         st.title('Welkom bij deze digitale quizmaster!')
         st.header('Belangrijke informatie:')
@@ -196,7 +198,7 @@ def main():
 
         st.markdown(audio_player, unsafe_allow_html=True)
     
-    if st.session_state.auth0 and st.session_state.auth1 and st.session_state.auth2 and st.session_state.auth3:
+    if not st.session_state.auth4 and (st.session_state.auth0 and st.session_state.auth1 and st.session_state.auth2 and st.session_state.auth3):
         # --- Placeholders ---
         viewport_placeholder = st.empty()
         controls_placeholder = st.empty()
@@ -261,13 +263,11 @@ def main():
             viewport_placeholder.pyplot(fig, use_container_width=True)
     
             # Doorgaan knop rechts onder
-            col1, col2, col3 = st.columns([3,3,1])
             clicked=controls_placeholder.button("Je hebt de uitgang gevonden! \nKlik hier om door te gaan.",key=4)
-            with col3:
-                if clicked:
-                    viewport_placeholder.empty()
-                    controls_placeholder.empty(f'Je hebt de escaperoom verlaten, GEFELICITEERD! \nJe tijd is {datetime.now().strftime("%H:%M")}.')
-    
+            if clicked:
+                viewport_placeholder.empty()
+                controls_placeholder.empty()
+                st.session_state.auth4=True
         else:
             # --- Mobielvriendelijke joystick ---
             with controls_placeholder.container():
@@ -300,6 +300,11 @@ def main():
             # --- Toon lokale viewport ---
             fig = show_viewport()
             viewport_placeholder.pyplot(fig, use_container_width=True)
+
+if st.session_state.auth4:
+    st.header('Einde escaperoom')
+    st.subheader('GEFELICITEERD! Dit was het einde van de escaperoom.')
+    st.write(f'Je hebt bij dezen de escaperoom verlaten. \nDe huidige tijd is {datetime.now().strftime("%H:%M")}. Heb je de elfjes op tijd kunnen helpen met het terugvinden van de verwarde Kerstman?')
     
 if __name__ == "__main__":
     main()
